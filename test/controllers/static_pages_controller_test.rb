e1b1669e79800d4d70b859097a503a318bf55686
MrDhillon <harman.singh.dhillon@gmail.com>
2015-11-11 04:09:39 -0500

    formatting

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,7 +19,7 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | Ruby on Rails"
   end
 
-  test "should" do
+  test "should get contact" do
     get :contact
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails"
