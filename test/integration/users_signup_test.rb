484a9bcb77c63b1a42c7d8e4ee19003d63291fa4
MrDhillon <harman.singh.dhillon@gmail.com>
2015-11-14 08:19:18 -0500

    error msg refactor

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,6 +15,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/new'
+    assert_select "div#error_explanation"
+    assert_select "div.alert-danger"
   end
 
   test "valid sign up" do
@@ -28,6 +30,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
   end
 
 end
