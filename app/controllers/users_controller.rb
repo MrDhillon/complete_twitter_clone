1f024fea309ff7400243df230f5652e8a8a48285
MrDhillon <harman.singh.dhillon@gmail.com>
2015-11-30 04:11:43 -0500

    site autherization complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only:[:edit, :update]
 
   def new
     @user = User.new
@@ -42,8 +43,13 @@ class UsersController < ApplicationController
 
   def logged_in_user
     if !logged_in?
-      flashed[:danger] = "Please login to continue"
+      flash[:danger] = "Please login to continue"
       redirect_to login_url
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -40,4 +40,9 @@ module SessionsHelper
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+
+  #returns true is the given user is the curent user
+  def current_user?(user)
+     user == current_user
+  end
 end
