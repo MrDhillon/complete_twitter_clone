147ce21d20fb067d67f4ec808b3bbe166d962806
MrDhillon <harman.singh.dhillon@gmail.com>
2015-12-14 09:19:28 -0500

    Add user micropost picture

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,16 @@
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       #Configuration for amazon s3
+#       :provider => 'AWS',
+#       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+#       :aws_secret_access_key => ENV['S3_SECERT_KEY']
+#     }
+#     config.fog_directory = ENV['S3_BUCKET']
+#   end
+# end
+
+# Access Key ID:
+# AKIAIWXH4IXL5364KPCA
+# Secret Access Key:
+# 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
